f4f47c5ec60e361ba6fd6eb911302672379546a5
Stick1000 <janleymolina@outlook.com>
2021-07-12 13:44:21 +0800

    Added modal binds

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,7 @@ const CARD = {
 
 /**
  * @param {string} layout - card layout as defined in the CARD constant (horizontal, vertical, or small)
+ * @param {boolean} includeModal - bind a modal to the Card item to show more details
  * @param {string} imgPath - path to image file to be displayed on the card
  * @param {string} title - title of the card
  * @param {string} subtitle - subtitle to be displayed on the card
@@ -19,10 +20,12 @@ const CARD = {
  */
 function bindToCard(
     layout,
+    includeModal,
     imgPath,
     title,
     subtitle,
     description,
+    showDescription,
     url,
     urlText = "More details"
 ) {
@@ -44,16 +47,19 @@ function bindToCard(
     }
 
     if (subtitle) {
-        clone.querySelector(`.${layout}-content .${layout}-subtitle`).textContent =
-            subtitle;
+        clone.querySelector(
+            `.${layout}-content .${layout}-subtitle`
+        ).textContent = subtitle;
     }
 
-    if (description) {
+    if (description && showDescription) {
         clone.querySelector(
             `.${layout}-content .${layout}-description`
         ).textContent = description;
     } else {
-        clone.querySelector(`.${layout}-content .${layout}-description`)?.remove();
+        clone
+            .querySelector(`.${layout}-content .${layout}-description`)
+            ?.remove();
     }
 
     if (url) {
@@ -67,11 +73,48 @@ function bindToCard(
 
     clone.classList.remove("template");
 
+    if (includeModal) {
+        bindToModal(clone, title, subtitle, description);
+    }
+
     return clone;
 }
 
-function bindToModal(element, modal) {}
+/**
+ * Modal
+ */
+var modal = document.getElementById("modal");
 
+modal
+    .querySelector(".modal-header .modal-close")
+    .addEventListener("click", () => (modal.hidden = true));
+
+modal.addEventListener("click", () => (modal.hidden = true));
+
+/**
+ * @param {Node} element - element that triggers the modal
+ * @param {string} title
+ * @param {string} subtitle
+ * @param {string} description
+ */
+
+function bindToModal(element, title, subtitle, description) {
+    if (!element) return false;
+
+    element.addEventListener("click", function (e) {
+        modal.querySelector(".modal-header .modal-title").textContent =
+            title || "No title";
+        modal.querySelector(".modal-content .modal-subtitle").textContent =
+            subtitle || "N/A";
+        modal.querySelector(".modal-content .modal-description").textContent =
+            description || "No description";
+        modal.hidden = false;
+    });
+}
+
+/**
+ * Binds the data to each subsection
+ */
 function bindData() {
     const _education = document.querySelector("#education .section-content");
     const _skills = document.querySelector("#skills .section-content");
@@ -90,11 +133,13 @@ function bindData() {
         education.forEach(function (e) {
             var card = bindToCard(
                 CARD.HORIZONTAL,
+                true,
                 e.img_path || PLACEHOLDER_IMAGE.EDUCATION,
                 e.name,
                 `${e.degree} \u2022 ${e.year_start} - ${
                     e.year_end || "Present"
-                }`
+                }`,
+                e.description
             );
 
             _education.append(card);
@@ -105,7 +150,7 @@ function bindData() {
         _skills.querySelector(".nodata").remove();
 
         skills.forEach(function (e) {
-            var card = bindToCard(CARD.SMALL, e.img_path, e.name);
+            var card = bindToCard(CARD.SMALL, false, e.img_path, e.name);
             _skills.append(card);
         });
     }
@@ -116,6 +161,7 @@ function bindData() {
         technologies.forEach(function (e) {
             var card = bindToCard(
                 CARD.HORIZONTAL,
+                false,
                 e.img_path || PLACEHOLDER_IMAGE.TECHNOLOGIES,
                 e.name,
                 e.level
@@ -130,10 +176,12 @@ function bindData() {
         projectsDone.forEach(function (e) {
             var card = bindToCard(
                 CARD.VERTICAL,
+                true,
                 e.img_path,
                 e.name,
                 `${e.date_started} - ${e.date_completed || "Present"}`,
                 e.description,
+                true,
                 e.url,
                 "Project link"
             );
@@ -146,6 +194,7 @@ function bindData() {
 
 bindData();
 
+// Handles button clicks on header navigation
 var details_btn = document.getElementById("details-btn");
 var about_btn = document.getElementById("about-btn");
 var return_btn = document.getElementById("return-btn");
