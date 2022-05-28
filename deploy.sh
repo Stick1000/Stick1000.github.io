6b2933b3c4378fd35e001dc476e8865eaf6ca45f
Stick1000 <janleymolina@outlook.com>
2022-05-28 19:10:48 +0800

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git add -A
+git commit -m 'Deploy to GitHub Pages'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:Stick1000/stick1000.github.io.git main
+
+cd -
